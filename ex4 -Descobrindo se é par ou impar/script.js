87dd91d00b32ed80014a43779b1c6011fdb652d4
luizlpf <44977537+luizlpf@users.noreply.github.com>
2022-01-02 19:40:06 -0300

    Update script.js

--- ex4 -Descobrindo se é par ou impar/script.js
+++ ex4 -Descobrindo se é par ou impar/script.js
@@ -1,4 +1,4 @@
-/*var question = prompt('Digite um numero');
+var question = prompt('Digite um numero');
 var answer = document.querySelector('.answer');
 
 if (question % 2 == 0) {
@@ -6,15 +6,3 @@ if (question % 2 == 0) {
 } else {
   alert(question + ' é numero impar ');
 }
-*/
-
-var num1 = prompt('Digite um numero 1');
-var num2 = prompt('Digite um numero 2');
-var answer = document.querySelector('.answer');
-
-if (num1 > num2) {
-  alert(num1 + ' é maior que o num2 ' + num2);
-} else {
-  alert(num2 + ' é maior que num1 ' + num1);
-}
-56;
